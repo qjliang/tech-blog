17691934f78b4a8577021e5893719c3a798570ff
qinjiliang <qinjiliang@patsnap.com>
2020-05-23 17:06:31 +0800

    change ip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,77 +1,72 @@
-
-import { Configuration } from '@nuxt/types/index';
+import { Configuration } from "@nuxt/types/index";
 
 const config: Configuration = {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: 'qBlog',
+    title: "qBlog",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#f80' },
+  loading: { color: "#f80" },
   /*
    ** Global CSS
    */
   css: [
-    'ant-design-vue/dist/antd.less',
-    '@fortawesome/fontawesome-svg-core/styles.css',
-    '~/static/main.css'
+    "ant-design-vue/dist/antd.less",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+    "~/static/main.css",
   ],
   /*
    ** Plugins to load before mounting the App
    */
   plugins: [
-    { src: '@/plugins/axios', mode: 'client' },
-    '@/plugins/ant-design',
-    '@/plugins/font-awesome',
-    '@/plugins/web-font/index',
-    '@/plugins/event-bus',
-    { src: '@/plugins/baidu-stats', mode: 'client' },
-    { src: '@/plugins/tui-editor', mode: 'client' }
+    { src: "@/plugins/axios", mode: "client" },
+    "@/plugins/ant-design",
+    "@/plugins/font-awesome",
+    "@/plugins/web-font/index",
+    "@/plugins/event-bus",
+    { src: "@/plugins/baidu-stats", mode: "client" },
+    { src: "@/plugins/tui-editor", mode: "client" },
   ],
-  buildModules: ['@nuxt/typescript-build'],
+  buildModules: ["@nuxt/typescript-build"],
   /*
    ** Nuxt.js modules
    */
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth',
-    '@nuxtjs/pwa'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth", "@nuxtjs/pwa"],
   auth: {
     strategies: {
       local: {
         endpoints: {
           login: {
-            url: '/api/auth/login',
-            method: 'post',
-            propertyName: 'token.accessToken'
+            url: "/api/auth/login",
+            method: "post",
+            propertyName: "token.accessToken",
           },
-          logout: { url: '/api/auth/logout', method: 'post' },
-          user: { url: '/api/auth/user', method: 'get', propertyName: 'user' }
-        }
-      }
+          logout: { url: "/api/auth/logout", method: "post" },
+          user: { url: "/api/auth/user", method: "get", propertyName: "user" },
+        },
+      },
     },
     redirect: {
-      login: '/auth/login',
-      logout: '/',
-      callback: '/auth/login',
-      home: '/'
-    }
+      login: "/auth/login",
+      logout: "/",
+      callback: "/auth/login",
+      home: "/",
+    },
   },
   /*
    ** Axios module configuration
@@ -79,8 +74,8 @@ const config: Configuration = {
    */
   axios: {
     // proxy: true,
-    baseURL: 'http://localhost:3001',
-    browserBaseURL: ''
+    baseURL: "http://192.168.31.63:3001",
+    browserBaseURL: "",
     // credentials: true
   },
   // proxy: {
@@ -93,22 +88,22 @@ const config: Configuration = {
     /*
      ** You can extend webpack config here
      */
-    extend () {},
+    extend() {},
     loaders: {
       less: {
         modifyVars: {
           // 'font-size-base': '16px',
-          'outline-width': '0'
+          "outline-width": "0",
         },
-        javascriptEnabled: true
-      }
-    }
+        javascriptEnabled: true,
+      },
+    },
   },
   typescript: {
     typeCheck: {
-      eslint: true
-    }
-  }
+      eslint: true,
+    },
+  },
   // watch: ['~/server']
 };
 
